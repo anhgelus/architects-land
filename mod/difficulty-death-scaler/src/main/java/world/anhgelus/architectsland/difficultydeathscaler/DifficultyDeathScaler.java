96ea110e4e227594f4fee2de2e069de853c7e40d
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-06-26 17:38:23 +0000

    feat(difficulty-death-scaler): increase difficulty with death

--- mod/difficulty-death-scaler/src/main/java/world/anhgelus/architectsland/difficultydeathscaler/DifficultyDeathScaler.java
+++ mod/difficulty-death-scaler/src/main/java/world/anhgelus/architectsland/difficultydeathscaler/DifficultyDeathScaler.java
@@ -2,7 +2,10 @@ package world.anhgelus.architectsland.difficultydeathscaler;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,9 +21,47 @@ public class DifficultyDeathScaler implements ModInitializer {
             if (!(entity instanceof ServerPlayerEntity)) {
                 return true;
             }
-            // final ServerPlayerEntity player = (ServerPlayerEntity) entity;
-            numberOfDeath++;
+            increaseDeath((ServerPlayerEntity) entity);
             return true;
         });
+    }
+
+    private void increaseDeath(ServerPlayerEntity player) {
+        numberOfDeath++;
+        updateDeath(player.getServerWorld().getServer());
+    }
+
+    private void decreaseDeath(ServerPlayerEntity player) {
+        if (numberOfDeath > 7) {
+            numberOfDeath = 5;
+        } else if (numberOfDeath > 5) {
+            numberOfDeath = 3;
+        } else if (numberOfDeath > 3) {
+            numberOfDeath = 1;
+        } else if (numberOfDeath > 1) {
+            numberOfDeath = 0;
+        }
+        updateDeath(player.getServerWorld().getServer());
+    }
+
+    private void updateDeath(MinecraftServer server) {
+        new GameRules().setAllValues(server.getGameRules(), server);
+        final var rules = server.getGameRules().copy();
+
+        server.getGameRules().setAllValues(rules, server);
+        switch (numberOfDeath) {
+            case 0:
+                server.setDifficulty(Difficulty.NORMAL, true);
+                server.getGameRules().get(GameRules.PLAYERS_SLEEPING_PERCENTAGE).set(30, server);
+            case 1:
+                server.getGameRules().get(GameRules.PLAYERS_SLEEPING_PERCENTAGE).set(70, server);
+            case 3:
+                server.setDifficulty(Difficulty.HARD, true);
+            case 5:
+                server.getGameRules().get(GameRules.PLAYERS_SLEEPING_PERCENTAGE).set(100, server);
+                server.getGameRules().get(GameRules.NATURAL_REGENERATION).set(true, server);
+            case 7:
+                server.getGameRules().get(GameRules.NATURAL_REGENERATION).set(false, server);
+        }
     }
 }

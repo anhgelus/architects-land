8b8f5e7a056ebba97bfd5656edb5e84cafc7b41f
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-06-26 21:21:56 +0000

    fix(difficulty-death-scaler): clean code and fix various bugs

--- mod/difficulty-death-scaler/src/main/java/world/anhgelus/architectsland/difficultydeathscaler/DifficultyDeathScaler.java
+++ mod/difficulty-death-scaler/src/main/java/world/anhgelus/architectsland/difficultydeathscaler/DifficultyDeathScaler.java
@@ -5,14 +5,15 @@ import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.GameRules;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -21,7 +22,7 @@ public class DifficultyDeathScaler implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     private int numberOfDeath = 0;
-    private long last = 0;
+    private long savedNumberOfDeath = 0;
 
     @Override
     public void onInitialize() {
@@ -37,13 +38,13 @@ public class DifficultyDeathScaler implements ModInitializer {
 
     private void increaseDeath(ServerPlayerEntity player) {
         numberOfDeath++;
-        last = System.currentTimeMillis() / 1000L;
+        savedNumberOfDeath = numberOfDeath;
         final var server = player.getServerWorld().getServer();
         final var timer = new Timer();
         final var reducer = new TimerTask() {
             @Override
             public void run() {
-                if (last - System.currentTimeMillis() / 1000L < 24*60*60*1000L-1) {
+                if (numberOfDeath != savedNumberOfDeath) {
                     timer.cancel();
                     return;
                 }
@@ -51,62 +52,61 @@ public class DifficultyDeathScaler implements ModInitializer {
                 if (numberOfDeath == 0) timer.cancel();
             }
         };
-        timer.schedule(reducer,24*60*60*1000L, 24*60*60*1000L);
-        updateDeath(server);
-        server.getPlayerManager().getPlayerList().forEach(p -> {
-            p.playSoundToPlayer(SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER, SoundCategory.AMBIENT, 1, 1);
-        });
+        timer.schedule(reducer,24*1000L, 24*1000L);
+        updateDeath(server, true);
     }
 
     private void decreaseDeath(MinecraftServer server) {
-        if (numberOfDeath > 7) {
+        if (numberOfDeath >= 7) {
             numberOfDeath = 5;
-        } else if (numberOfDeath > 5) {
+        } else if (numberOfDeath >= 5) {
             numberOfDeath = 3;
-        } else if (numberOfDeath > 3) {
+        } else if (numberOfDeath >= 3) {
             numberOfDeath = 1;
-        } else if (numberOfDeath > 1) {
+        } else if (numberOfDeath >= 1) {
             numberOfDeath = 0;
         }
-        updateDeath(server);
+        savedNumberOfDeath = numberOfDeath;
+        updateDeath(server, false);
     }
 
-    private void updateDeath(MinecraftServer server) {
-        new GameRules().setAllValues(server.getGameRules(), server);
-        final var rules = server.getGameRules().copy();
+    private void updateDeath(@NotNull MinecraftServer server, boolean playSound) {
+        final var gamerules = server.getGameRules();
+        final var sleeping = gamerules.get(GameRules.PLAYERS_SLEEPING_PERCENTAGE);
+        final var naturelRegeneration = gamerules.get(GameRules.NATURAL_REGENERATION);
 
-        server.getGameRules().setAllValues(rules, server);
-        Difficulty difficulty = null;
-        switch (numberOfDeath) {
-            case 0:
-                server.setDifficulty(Difficulty.NORMAL, true);
-                server.getGameRules().get(GameRules.PLAYERS_SLEEPING_PERCENTAGE).set(30, server);
-                difficulty = Difficulty.NORMAL;
-                break;
-            case 1:
-                server.getGameRules().get(GameRules.PLAYERS_SLEEPING_PERCENTAGE).set(70, server);
-                difficulty = Difficulty.NORMAL;
-                break;
-            case 3:
-                server.setDifficulty(Difficulty.HARD, true);
-                difficulty = Difficulty.HARD;
-                break;
-            case 5:
-                server.getGameRules().get(GameRules.PLAYERS_SLEEPING_PERCENTAGE).set(100, server);
-                server.getGameRules().get(GameRules.NATURAL_REGENERATION).set(true, server);
-                difficulty = Difficulty.HARD;
-                break;
-            case 7:
-                server.getGameRules().get(GameRules.NATURAL_REGENERATION).set(false, server);
-                difficulty = Difficulty.HARD;
-                break;
+        Difficulty difficulty = Difficulty.NORMAL;
+        if (numberOfDeath >= 7) {
+            naturelRegeneration.set(false, server);
+            sleeping.set(100, server);
+            difficulty = Difficulty.HARD;
+        } else if (numberOfDeath >= 5) {
+            naturelRegeneration.set(true, server);
+            sleeping.set(100, server);
+            difficulty = Difficulty.HARD;
+        } else if (numberOfDeath >= 3) {
+            naturelRegeneration.set(true, server);
+            sleeping.set(70, server);
+            difficulty = Difficulty.HARD;
+        } else if (numberOfDeath >= 1) {
+            naturelRegeneration.set(true, server);
+            sleeping.set(70, server);
+        } else if (numberOfDeath == 0) {
+            naturelRegeneration.set(true, server);
+            sleeping.set(30, server);
         }
-        if (difficulty != null) {
+        if (List.of(0,1,3,5,7).contains(numberOfDeath)) {
+            server.setDifficulty(difficulty, true);
             server.getPlayerManager().broadcast(Text.of(generateDifficultyUpdate(server, difficulty)), false);
+            if (playSound) {
+                server.getPlayerManager().getPlayerList().forEach(p -> {
+                    p.playSoundToPlayer(SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER, SoundCategory.AMBIENT, 1, 1.2f);
+                });
+            }
         }
     }
 
-    private String generateDifficultyUpdate(MinecraftServer server, Difficulty difficulty) {
+    private @NotNull String generateDifficultyUpdate(@NotNull MinecraftServer server, Difficulty difficulty) {
         final var gamerules = server.getGameRules();
         final var percentage = gamerules.get(GameRules.PLAYERS_SLEEPING_PERCENTAGE).get();
         final var naturalRegeneration = gamerules.get(GameRules.NATURAL_REGENERATION).get();
